d3a7742ec9a918a642da2a8ef0e58ad34449bc02
pgiraudoux <patrick.giraudoux@univ-fcomte.fr>
2018-05-10 16:36:30 +0200

    Bug in CI fixed
    CI could not handle data.frame. This has been fixed

--- pgirmess/R/CI.r
+++ pgirmess/R/CI.r
@@ -1,5 +1,6 @@
 "CI"<-function(x, ...){
     if(!(is.matrix(x) | is.data.frame(x) | is.table(x)) & ncol(x)==2) stop("x must be a two column matrix, table or data.frame")
+    if(is.data.frame(x)) x<-as.matrix(x)
     bashau <- cbind(rep(0,nrow(x)),rep(0, nrow(x)), rep(0, nrow(x)))
     for(i in 1:nrow(x)) {
         ic <- prop.test(x[i,,drop=FALSE],...)$conf.int
